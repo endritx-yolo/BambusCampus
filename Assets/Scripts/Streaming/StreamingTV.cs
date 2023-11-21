3baf0ef3b5454bf07aff9f8492da8c160949efda
PlayerTtwo <leon.nuredini@treedev.io>
2023-11-21 13:38:56 +0100

    Simlated spatial audio

--- Assets/Scripts/Streaming/StreamingTV.cs
+++ Assets/Scripts/Streaming/StreamingTV.cs
@@ -1,9 +1,30 @@
-using PlayerActions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace VideoStreaming
 {
     public class StreamingTV : StreamingObject
     {
+        public Vector3 GetWorldPosition() => transform.position;
+
+        private void OnEnable()
+        {
+            TVHolder.OnAddToCollection += TVHolder_OnAddToCollection;
+        }
+
+        private void OnDisable()
+        {
+            TVHolder.OnAddToCollection -= TVHolder_OnAddToCollection;
+        }
+
+        private List<StreamingTV> TVHolder_OnAddToCollection(List<StreamingTV> streamingTVList)
+        {
+            streamingTVList.Add(this);
+            Debug.LogWarning(this);
+            return streamingTVList;
+        }
+
         public override bool TryTogglePlayPause()
         {
             if (!_canBePaused) return false;
