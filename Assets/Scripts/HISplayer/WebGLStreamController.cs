bf0f5b3ae502cc0cf940f80f5e445c56b54f1238
PlayerTtwo <leon.nuredini@treedev.io>
2023-11-27 13:03:40 +0100

    Made some minor changes

--- Assets/Scripts/HISplayer/WebGLStreamController.cs
+++ Assets/Scripts/HISplayer/WebGLStreamController.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.IO;
 using System.Net;
-using System.Net.Http;
-using System.Text;
 using HISPlayerAPI;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -15,21 +13,21 @@ namespace VideoStreaming
         private static WebGLStreamController _instance;
         public static WebGLStreamController Instance => _instance;
 
-        private const string _staticPath = "D:\\Unity Repos\\BambusCampus\\Builds\\StreamingAssets\\properties.json";
-
-        private const string _urlPath =
+        private const string _ftpUrlPath =
             @"ftp://endrit.xhemaili%2540yolo-way.com@92.205.8.194/unity_showrooms/bambus_hisplayer_showroom/livestream.txt";
 
+        private const string _httpUrlPath = @"https://57ae-84-22-36-74.ngrok-free.app";
+        
         private VideoStreamingProperties _videoStreamingProperties;
 
+        [SerializeField] private RequestType _requestType = RequestType.FTP;
+
+        private string _result = String.Empty;
+        private bool _isStreamSetUp;
+        
         protected override void Awake()
         {
-            //ReadJsonFile();
-            //byte[] byteArray = ReadUrlHTTPRequest();
-            //string textstring = Convert.ToBase64String(text);
-            
-            string result = ReadUrlHTTPRequest();
-
+            /*string result = ReadUrlFTPRequest();
             if (_instance == null)
                 _instance = this;
             else
@@ -37,32 +35,55 @@ namespace VideoStreaming
                 Destroy(gameObject);
                 return;
             }
-
             
             for (int i = 0; i < multiStreamProperties.Count; i++)
                 multiStreamProperties[i].url[0] = result;
             
-
             base.Awake();
+            SetUpPlayer();*/
+            
+             if (_instance == null)
+                _instance = this;
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+             base.Awake();
+
+            /*switch (_requestType)
+            {
+                case RequestType.FTP:
+                    _result = ReadUrlFTPRequest();
+                    Debug.LogError(_result);
+                    break;
+                case RequestType.HTTP:
+                    StartCoroutine(ReadUrlHTTPRequest(SetUpHisPlayer));
+                    return;
+                    //_result = ReadUrlHTTPWebRequest();
+                    break;
+                case RequestType.None:
+                    break;
+            }*/
+
+            SetUpHisPlayer();
+        }
+
+        private void SetUpHisPlayer()
+        {
+            if (_result != String.Empty)
+                for (int i = 0; i < multiStreamProperties.Count; i++)
+                    multiStreamProperties[i].url[0] = _result;
+            
             SetUpPlayer();
+            _isStreamSetUp = true;
         }
 
         private void Start() => SetNewVolume(1f);
 
-        private void ReadJsonFile()
+        private string ReadUrlFTPRequest()
         {
-            string filePath = Path.Combine(Application.streamingAssetsPath, "properties.json");
-#if UNITY_WEBGL
-            filePath = filePath.Replace("/", "");
-            filePath = _staticPath;
-#endif
-            string dataAsJson = File.ReadAllText(filePath);
-            _videoStreamingProperties = JsonUtility.FromJson<VideoStreamingProperties>(dataAsJson);
-        }
-
-        private string ReadUrlHTTPRequest()
-        {
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(_urlPath));
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(_ftpUrlPath));
 
             request.UsePassive = true;
             request.UseBinary = true;
@@ -71,29 +92,64 @@ namespace VideoStreaming
             request.Credentials = new NetworkCredential("endrit.xhemaili@yolo-way.com", "k2){^Kp=^}pN");
             request.Method = WebRequestMethods.Ftp.DownloadFile;
 
-            return DownloadAsByteArray(request.GetResponse());
-
-            /*UnityWebRequest myWr = UnityWebRequest.Get(_urlPath);
-            yield return myWr.SendWebRequest();
-
-            if (myWr.result != UnityWebRequest.Result.Success)
+            try
             {
-                Debug.LogWarning(myWr.error);
-                yield break;
+                var response = request.GetResponse();
+                return DownloadAsByteArray(response);
             }
-            Debug.LogWarning(myWr.downloadHandler.text);
-            byte[] results = myWr.downloadHandler.data;
-            for (int i = 0; i < results.Length; i++)
+            catch (WebException e)
             {
-                Debug.LogWarning(results[i].ToString());
-            }*/
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private IEnumerator ReadUrlHTTPRequest(Action callback)
+        {
+            string urlPath = $"{_httpUrlPath}/livestreamlink/LivestreamUrl.txt";
+            UnityWebRequest webRequest = UnityWebRequest.Get(urlPath);
+            webRequest.SetRequestHeader("Access-Control-Allow-Credentials", "true");
+            webRequest.SetRequestHeader("Access-Control-Expose-Headers", "Content-Length, Content-Encoding");
+            webRequest.SetRequestHeader("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time, Content-Type");
+            webRequest.SetRequestHeader("Access-Control-Allow-Method", "GET, POST, OPTIONS");
+            webRequest.SetRequestHeader("Access-Control-Allow-Origin", "*");
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogWarning(webRequest.error);
+            }
+            else
+            {
+                _result = webRequest.downloadHandler.text;
+                Debug.LogWarning("Stream Link: " + _result);
+                callback?.Invoke();
+            }
+
+            webRequest.Dispose();
+        }
+
+        private string ReadUrlHTTPWebRequest()
+        {
+            string strContent = String.Empty;
+            string urlPath = $"{_httpUrlPath}/livestreamlink/LivestreamUrl.txt";
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(new Uri(urlPath));
+
+            var response = webRequest.GetResponse();
+            var content = response.GetResponseStream();
+            
+            return DownloadAsByteArray(webRequest.GetResponse());
+            
+            using (var reader = new StreamReader(content))
+            {
+                strContent = reader.ReadToEnd();
+                return strContent;
+            }
         }
 
         private string DownloadAsByteArray(WebResponse request)
         {
             using (Stream input = request.GetResponseStream())
             {
-                byte[] buffer = new byte[16 * 1024];
                 using (StreamReader reader = new StreamReader(input))
                 {
                     string fileContent = reader.ReadToEnd();
@@ -109,6 +165,7 @@ namespace VideoStreaming
 
         public void PlayStream(int streamIndex)
         {
+            if (!_isStreamSetUp) return;
             if (streamIndex >= multiStreamProperties.Count)
             {
                 Debug.LogError(
@@ -122,6 +179,7 @@ namespace VideoStreaming
 
         public void PauseStream(int streamIndex)
         {
+            if (!_isStreamSetUp) return;
             if (streamIndex >= multiStreamProperties.Count)
             {
                 Debug.LogError(
@@ -135,6 +193,7 @@ namespace VideoStreaming
 
         public void SetNewTrackURL(int streamIndex, string newUrl)
         {
+            if (!_isStreamSetUp) return;
             if (streamIndex >= multiStreamProperties.Count)
             {
                 Debug.LogError(
@@ -147,12 +206,14 @@ namespace VideoStreaming
 
         public void StopAllLiveStreams()
         {
+            if (!_isStreamSetUp) return;
             for (int i = 0; i < multiStreamProperties.Count; i++)
                 Stop(i);
         }
 
         public void SetNewVolume(float volume)
         {
+            if (!_isStreamSetUp) return;
             volume = Mathf.Clamp(volume, 0f, 1f);
             for (int i = 0; i < multiStreamProperties.Count; i++)
                 SetVolume(i, volume);
@@ -160,24 +221,27 @@ namespace VideoStreaming
 
         public void SetNewVolume(int streamIndex, float volume)
         {
+            if (!_isStreamSetUp) return;
             if (streamIndex >= multiStreamProperties.Count)
             {
                 Debug.LogError(
                     $"Stream index '{streamIndex}' is greater or equals to the total stream count of {multiStreamProperties.Count}.");
                 return;
             }
-            
+
             SetVolume(streamIndex, volume);
         }
 
         public void ChangeSpeedRate(float newSpeed)
         {
+            if (!_isStreamSetUp) return;
             for (int i = 0; i < multiStreamProperties.Count; i++)
                 SetPlaybackSpeedRate(i, newSpeed);
         }
 
         public void ToggleSubtitles(int streamIndex, int ccTrackIndex)
         {
+            if (!_isStreamSetUp) return;
             if (streamIndex >= multiStreamProperties.Count)
             {
                 Debug.LogError(
